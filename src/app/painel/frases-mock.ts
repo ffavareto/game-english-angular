534c873cd5223f1963dd86352125de4d8b7321e1
Lucas Santos <lucas.santos@grupoiv2.local>
2020-06-29 20:27:44 -0300

    progress bar

--- src/app/painel/frases-mock.ts
+++ src/app/painel/frases-mock.ts
@@ -1,7 +1,7 @@
 import { Frase } from '../shared/frase.model'
 
-const FRASES: Frase[] = [
-  { fraseEng: 'I lake to learn', frasePtBr: 'Eu gosto de aprender' },
+export const FRASES: Frase[] = [
+  { fraseEng: 'I like to learn', frasePtBr: 'Eu gosto de aprender' },
   { fraseEng: 'I watch tv', frasePtBr: 'Eu assisto tv' },
   { fraseEng: 'How are you?', frasePtBr: 'Como vai você?' },
   { fraseEng: 'I eat bread', frasePtBr: 'Eu como pão' }
--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Frase } from '../shared/frase.model'
+import { FRASES } from './frases-mock'
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-painel',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  public frases: Frase[] = FRASES
+  public instrucao: string = 'Traduza a frase'
+  public resposta: string
+
+  public rodada: number = 0
+  public rodadaFrase: Frase
+
+  public progresso: number = 0
+
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada]
+  }
 
   ngOnInit(): void {
   }
 
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value
+    // console.log(this.resposta);
+  }
+
+  public verificaResposta(): void {
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+      alert('ok')
+      this.rodada++
+      this.progresso = this.progresso + (100 / this.frases.length)
+      this.rodadaFrase = this.frases[this.rodada]
+    } else {
+      alert('errastes')
+    }
+  }
 }
